8d7e06356d8623ce0277317789485f63a80aab53
Ignacio Becerra <i1becerr@ucsd.edu>
2018-11-08 17:49:08 -0800

    Made the javascript work

--- js/showResults.js
+++ js/showResults.js
@@ -1,8 +1,8 @@
-var test = {'title': 'National Tree 7 Foot Dunhill Fir Tree (DUH-70)',
-			'itemImage': './assets/imgs/cow-brick.jpg',
-			'price': '$8',
-			'index': 1,
-			'store': 'UglyStore'};
+var test = {'Description': 'National Tree 7 Foot Dunhill Fir Tree (DUH-70)',
+			'Link': './assets/imgs/cow-brick.jpg',
+			'Price': '$8',
+			'Index': 1,
+			'Store': 'UglyStore'};
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
@@ -12,12 +12,17 @@ $(document).ready(function() {
 	var source = $("#results-template").html();
 	var template = Handlebars.compile(source);
 
-	var parentDiv = $("#templatedResult");
+	var parentDiv = $(".grid");
 
 	var html = template(test);
 	console.log(html);
 	parentDiv.append(html);
 
+  for (var i = 0; i < local_data.length; i++) {
+      var curData = local_data[i];
+      var curHtml = template(curData);
+      parentDiv.append(curHtml);
+  }
   
   // Use the URLSearchParams API to make fake-database queries using a URL
   // https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams

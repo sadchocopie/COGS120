e79c6d83b4721bb901432be5ce00220715193884
IgnacioBecerra <i1becerr@ucsd.edu>
2018-11-17 20:06:50 -0800

    Added sorting by rating

--- js/sorting.js
+++ js/sorting.js
@@ -5,17 +5,32 @@ document.getElementById('sortingMenuId').addEventListener('change', function (e)
     var boxArray = [];
     var priceArray = [];
 
+    var price;
+    var parentBox;
+    var review;
+    var set;
+
     // Populate array with grid contents
     for(var i =0; i < results.length; i++) {
-       
+    
        // Get price tag to analyze
        if(results[i].id === "itemPrice") {
-            var price = parseFloat(results[i].innerHTML.replace(/\$|,/g, ''));
-            var parentBox = results[i].parentElement.parentElement;
+            price = parseFloat(results[i].innerHTML.replace(/\$|,/g, ''));
+            parentBox = results[i].parentElement.parentElement;
+       } 
 
-            boxArray.push({price:price, parent:parentBox});
+       // get rating
+       if(results[i].id === "ratingScore") {
+            review = results[i].innerHTML;
+            set = true;
        }
 
+       // only add when price and ratings are set
+       if(set === true) {
+
+            boxArray.push({parent:parentBox, price:price, rating:review});
+            set = false;
+        }
     }
 
     if(e.target.value === "Price-lowToHigh") {
@@ -41,10 +56,29 @@ document.getElementById('sortingMenuId').addEventListener('change', function (e)
 
         });
 
-    } else if(e.target.value === "Rating") {
+    } else if(e.target.value === "Rating-lowToHigh") {
+        boxArray.sort(function (b1, b2) {
 
+            // Sort by price
+            // If the first item has a higher number, move it down
+            // If the first item has a lower number, move it up
+            if (b1.rating <= b2.rating) return -1;
+            if (b1.rating > b2.rating) return 1;
+
+        });
+    } else if(e.target.value === "Rating-highToLow") {
+        boxArray.sort(function (b1, b2) {
+
+            // Sort by price
+            // If the first item has a higher number, move it up
+            // If the first item has a lower number, move it down
+            if (b1.rating >= b2.rating) return -1;
+            if (b1.rating < b2.rating) return 1;
+
+        });
     }
     document.getElementsByClassName("grid")[0].innerHTML = '';
+    console.log(boxArray);
 
     for(var i = 0; i < boxArray.length; i++) {
         document.getElementsByClassName("grid")[0].appendChild(boxArray[i].parent);
